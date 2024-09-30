ab06ec94ae65876dcd39fda740cc3bd2b0e2df01
Andrew Fawcett <ajfawcett@gmail.com>
2024-09-30 10:45:01 -0500

    Edit Finals

--- IntegrationByParts/IntByParts.py
+++ IntegrationByParts/IntByParts.py
@@ -8,49 +8,30 @@ class IntByParts(Scene):
         self.play(Write(title, run_time=1), Write(colored_title, run_time=1.4))
         self.remove(title) 
         self.play(Transform(colored_title, title_top))
-
+        self.wait(2)
         # Step 1a: Write the derivative of the product
         step1a = MathTex(r"\frac{d}{dx}\left[ f(x)g(x) \right]")
         self.play(Write(step1a))
-        
+        self.wait(2)
         # Step 1b: Fade out the left side and show the product rule result
         step1b = MathTex(r"f'(x)g(x) + f(x)g'(x)")
         self.play(Transform(step1a, step1b))
-
-        # Step 2a: Integrate both sides, starting with the LHS
-        step2a = MathTex(r"\int \frac{d}{dx}\left[ f(x)g(x) \right] \, dx")
-        self.play(Transform(step1a, step2a))
-        
-        # Step 2b: Transition to the integrated version on the right side
-        step2b = MathTex(r"\int f'(x)g(x) \, dx + \int f(x)g'(x) \, dx")
-        self.play(Transform(step1a, step2b))
-
-        
-        # Step 3a: LHS simplifies using the fundamental theorem
-        step3a = MathTex(r"f(x)g(x)")
-        self.play(Transform(step1a, step3a))
-
+        self.wait(2)
 
         # Step 3b: Transition to the integrated terms on the right side
-        step3b = MathTex(r"f(x)g(x) = \int f'(x)g(x) \, dx + \int f(x)g'(x) \, dx")
+        step3b = MathTex(r" \int f'(x)g(x) \, dx + \int f(x)g'(x) \, dx")
         self.play(Transform(step1a, step3b))
-
-        # Step 4a: Isolate \int f(x)g'(x) \, dx on the left side
-        step4a = MathTex(r"\int f(x)g'(x) \, dx")
-        self.play(Transform(step1a, step4a))
-
+        self.wait(2)
         # Step 4b: Move the remaining terms to the right side
         step4b = MathTex(r"f(x)g(x) - \int f'(x)g(x) \, dx")
         self.play(Transform(step1a, step4b))
+        self.wait(2)
 
-        # Step 5a: Substituting u and dv (LHS)
-        step5a = MathTex(r"\int u \, dv")
-        self.play(Transform(step1a, step5a))
 
         # Step 5b: Transition to the final integration by parts formula on the right
         step5b = MathTex(r"uv - \int v \, du")
         self.play(Transform(step1a, step5b))
 
         # Optional: Fade out to finish
-        self.play(FadeOut(step1a))
+        self.wait(2)
 
